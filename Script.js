b9e855693472d1bde0e5e1b5d88c0aa640388983
Anderson Vanzetto <vanzettoanderson@gmail.com>
2019-04-28 23:21:56 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+  var x = document.getElementById("conteudo");
+  if (window.getComputedStyle(x).display === "none") {
+    x.style.display = "block";
+  }
+
+}
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
