e1197bd4494848d944dacfb435e5eed768ac0a87
Anderson Vanzetto <vanzettoanderson@gmail.com>
2019-05-02 21:19:00 -0300

    Merge branch 'master' of https://github.com/AndersonVanzetto/fakenews

--- Script.js
+++ Script.js
@@ -17,17 +17,31 @@ function openFullscreen() {
     elem.msRequestFullscreen();
   }
 }
-var slideIndex = 1;
-showSlides(slideIndex);
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-  var slideIndex = 1;
-  showSlides(slideIndex);
-}
 function ocultar()
 {
    var camada=document.getElementById("limpar");
    camada.style.display="none";
    camada.style.visibility="hidden";
+}
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
